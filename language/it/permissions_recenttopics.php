8ddcf7f6fb1b8576aa8bd4c30d7f59137fd358c9
Mauron <Mauron@users.noreply.github.com>
2015-09-23 22:28:07 +0200

    Update for Italian translation for permissions_recenttopics.php
    Harmonisation with the official Italian phpBB translation.

--- language/it/permissions_recenttopics.php
+++ language/it/permissions_recenttopics.php
@@ -21,9 +21,9 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACL_U_RT_VIEW'            => 'Può nascondere l’elenco dei topic recenti',
-	'ACL_U_RT_ENABLE'          => 'Può abilitare l’elenco dei topic recenti',
-	'ACL_U_RT_ALT_LOCATION'    => 'Può mostrare l’elenco dei topic recenti nella posizione alternativa',
-	'ACL_U_RT_SORT_START_TIME' => 'Può cambiare il metodo di ordinamento dei topic recenti',
-	'ACL_U_RT_UNREAD_ONLY'     => 'Può visualizzare solo i topic non letti nell’elenco dei topic recenti',
+	'ACL_U_RT_VIEW'            => 'Può nascondere l’elenco di argomenti recenti',
+	'ACL_U_RT_ENABLE'          => 'Può abilitare l’elenco di argomenti recenti',
+	'ACL_U_RT_ALT_LOCATION'    => 'Può mostrare l’elenco di argomenti recenti in posizione alternativa',
+	'ACL_U_RT_SORT_START_TIME' => 'Può cambiare metodo di ordinamento di argomenti recenti',
+	'ACL_U_RT_UNREAD_ONLY'     => 'Può visualizzare solo argomenti non letti nell’elenco di argomenti recenti',
 ));
