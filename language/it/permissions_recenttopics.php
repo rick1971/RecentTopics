a8d2b2c80cd06cbf39815c43bcba2facf266d007
Mauron <Mauron@users.noreply.github.com>
2015-12-20 12:48:50 +0100

    Italian translation for permissions_recenttopics.php

--- language/it/permissions_recenttopics.php
+++ language/it/permissions_recenttopics.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package Recent Topics Extension
- * English translation by PayBas
+ * Italian translation by Mauron
  *
  * @copyright (c) 2015 PayBas
  * @license GNU General Public License, version 2 (GPL-2.0)
@@ -21,9 +21,9 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACL_U_RT_VIEW'            => 'Recent Topics: view (master)',
-	'ACL_U_RT_ENABLE'          => 'Recent Topics: enable or disable',
-	'ACL_U_RT_ALT_LOCATION'    => 'Recent Topics: use alternative display location',
-	'ACL_U_RT_SORT_START_TIME' => 'Recent Topics: change sort order',
-	'ACL_U_RT_UNREAD_ONLY'     => 'Recent Topics: use unread-only mode',
+	'ACL_U_RT_VIEW'            => 'Può visualizzare l’elenco di argomenti recenti', //'Argomenti recenti: mostra (master)',
+	'ACL_U_RT_ENABLE'          => 'Può disattivare l’elenco di argomenti recenti', // 'Argomenti recenti: attiva o disattiva',
+	'ACL_U_RT_ALT_LOCATION'    => 'Può visualizzare l’elenco di argomenti recenti in posizione alternativa', // 'Argomenti recenti: usa posizione alternativa',
+	'ACL_U_RT_SORT_START_TIME' => 'Può cambiare la modalità di ordinamento dell’elenco di argomenti recenti', // 'Argomenti recenti: cambia ordinamento',
+	'ACL_U_RT_UNREAD_ONLY'     => 'Può impostare la visualizzazione dei soli argomenti non letti nell’elenco di argomenti recenti', // 'Argomenti recenti: usa modalità “solo non letti”',
 ));
