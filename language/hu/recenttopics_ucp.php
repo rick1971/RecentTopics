7cd747fdbcbbbf5848a9a6fd1e56793f8866cdf4
Sajaki <Sajaki9@gmail.com>
2016-03-12 12:11:17 +0100

    Croatian translation (Ancica) Hungarian translation (aszilagyi) permissions and ucp file were translated from google translate.

---
+++ language/hu/recenttopics_ucp.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * @package Recent Topics Extension
+ * Hungarian translation by PayBas
+ *
+ * @copyright (c) 2015 PayBas
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * Based on the original NV Recent Topics by Joas Schilling (nickvergessen)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'RT_ENABLE'              => 'A kijelző Friss témák',
+	'RT_ALT_LOCATION'        => 'Az alternatív megjelenítési helyét',
+	'RT_ALT_LOCATION_EXP'    => 'Mozgás utóbbi téma egy másik helyre (általában az alján, de attól függ, hogy a stílus használható) .',
+	'RT_SORT_START_TIME'     => 'Rendezés Legújabb témák téma kezdési időpont',
+	'RT_SORT_START_TIME_EXP' => 'Rendezés helyett ezek az utolsó üzenet ideje.',
+	'RT_UNREAD_ONLY'         => 'Csak megjelenítéséhez olvasatlan téma az utóbbi témák',
+));
