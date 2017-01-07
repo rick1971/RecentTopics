a5fa3e0df20e416a30a02061f008b98214d385af
Sajaki <Sajaki9@gmail.com>
2017-01-07 19:26:03 +0100

    update lang from 3.1

--- language/ru/recenttopics_ucp.php
+++ language/ru/recenttopics_ucp.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package Recent Topics Extension
- * Tradução Portuguesa by phpbbpt
+ * Russian translation by HD321kbps
  *
  * @copyright (c) 2015 PayBas
  * @license GNU General Public License, version 2 (GPL-2.0)
@@ -37,14 +37,14 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge(
 	$lang, array(
-	'RT_ENABLE'              => 'Exibir tópicos recentes',
-	'RT_LOCATION'            => 'Selecionar localização',
-	'RT_LOCATION_EXP'        => 'Selecionar local para exibir tópicos recentes.',
-	'RT_SORT_START_TIME'     => 'Ordenar tópicos recentes por tópico hora de início',
-	'RT_SORT_START_TIME_EXP' => 'Em vez de classificá-los por último tempo de postagem',
-	'RT_UNREAD_ONLY'         => 'Mostrar apenas tópicos não lidos nos tópicos recentes',
-	'RT_TOP'                 => 'Mostrar no topo',
-	'RT_BOTTOM'              => 'Mostrar no fundo',
-	'RT_SIDE'                => 'Mostrar no lado direito',
+	'RT_ENABLE'              => 'Показывать последние темы',
+	'RT_LOCATION' 			 => 'Расположение последних тем',
+	'RT_LOCATION_EXP' 		 => 'Выберите расположения для отображения последних тем.',
+	'RT_SORT_START_TIME'	 => 'Сортировать по дате создания',
+	'RT_SORT_START_TIME_EXP' => 'Если включено, темы будут отсортированы по дате их создания, а не по дате последнего сообщения.',
+	'RT_UNREAD_ONLY'		 => 'Только непрочтённые',
+	'RT_TOP' 				 => 'Показывать верху',
+	'RT_BOTTOM' 			 => 'Показывать внизу',
+	'RT_SIDE' 				 => 'Показывать сбоку',
 	)
 );
